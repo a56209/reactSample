84886a5cbb35d7b63b1a26487ce2eb49c7e26c88
a56209 <41112394@qq.com>
2019-06-03 18:35:09 +0800

    button

--- src/pages/ui/buttons.js
+++ src/pages/ui/buttons.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import {Card, Button} from 'antd'
+import './ui.less'
 
 export default class Buttons extends Component {
     render() {
         return (
             <div>
-                this is Buttons page.
+                <Card title ="基础按钮" className="card-wrap">
+                    <Button type="primary">顺欣同创</Button>
+                    <Button >顺欣同创</Button>
+                    <Button type="dashed">顺欣同创</Button>
+                    <Button type="danger">顺欣同创</Button>
+                    <Button disabled>顺欣同创</Button>
+                </Card>
+                <Card title="图形按钮" className="card-wrap">
+                    <Button icon="plus">创建</Button>
+                    <Button icon="edit">编辑</Button>
+                    <Button icon="delete">删除</Button>
+                    <Button shape="circle" icon="search"></Button>
+                    <Button type="primary" icon="search">搜索</Button>
+                    <Button type="primary" icon="download">下载</Button>
+                </Card>
             </div>
         );
     }
