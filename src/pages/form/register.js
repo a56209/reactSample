52fdb5c283ddec30509a7e4d07124f081fb3b0dd
a56209 <41112394@qq.com>
2019-06-18 18:40:36 +0800

    注册页面01

--- src/pages/form/register.js
+++ src/pages/form/register.js
@@ -1,11 +1,166 @@
 import React, { Component } from 'react'
-import {Card,Form} from 'antd'
+import { Card, Form, Button, Input, Checkbox, Radio, Select, Switch, DatePicker, TimePicker, Upload, Icon, message, InputNumber } from 'antd'
+import moment from 'moment'
+const FormItem = Form.Item
+const RadioGroup = Radio.Group
+const Option = Select.Option
+const TextArea = Input.TextArea
 
-class FormRegister extends Component{
-    render(){
+class FormRegister extends Component {    
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        const formItemLayout={
+            labelCol:{
+                xs:24,
+                sm:4
+            },
+            wrapperCol:{
+                xs:24,
+                sm:12
+            }
+        }
+        const rowObject = {
+            minRows:4,maxRows:6
+        }
         return (
             <div>
-                注册页面
+                <Card title="注册表单">
+                    <Form layout="horizontal">
+                        <FormItem label="用户名" {...formItemLayout}>
+                            {
+                                getFieldDecorator('userName', {
+                                    initialValue: '',
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '用户名不能为空'
+                                        }
+                                    ]
+                                })(
+                                    <Input placeholder="请输入用户名" />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="密码" {...formItemLayout}>
+                            {
+                                getFieldDecorator('userPwd', {
+                                    initialValue: ''
+                                })(
+                                    <Input type="password" placeholder="请输入密码" />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="性别" {...formItemLayout}>
+                            {
+                                getFieldDecorator('sex', {
+                                    initialValue: ''
+                                })(
+                                    <RadioGroup>
+                                        <Radio value="1">男</Radio>
+                                        <Radio value="2">女</Radio>
+                                    </RadioGroup>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="年龄" {...formItemLayout}>
+                            {
+                                getFieldDecorator('age', {
+                                    initialValue: 18
+                                })(
+                                    <InputNumber/>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="当前状态" {...formItemLayout}>
+                            {
+                                getFieldDecorator('state', {
+                                    initialValue: '2'
+                                })(
+                                    <Select>
+                                        <Option value="1">幼儿园</Option>
+                                        <Option value="2">小学</Option>
+                                        <Option value="3">中学</Option>
+                                        <Option value="4">高中</Option>
+                                        <Option value="5">大学</Option>
+                                    </Select>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="爱好" {...formItemLayout}>
+                            {
+                                getFieldDecorator('interest', {
+                                    initialValue: ['2','5']
+                                })(
+                                    <Select mode="multiple">
+                                        <Option value="1">游泳</Option>
+                                        <Option value="2">打篮球</Option>
+                                        <Option value="3">踢足球</Option>
+                                        <Option value="4">跑步</Option>
+                                        <Option value="5">爬山</Option>
+                                        <Option value="6">骑行</Option>
+                                        <Option value="7">桌球</Option>
+                                        <Option value="8">麦霸</Option>
+                                    </Select>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="是否已婚" {...formItemLayout}>
+                            {
+                                getFieldDecorator('isMarried', {
+                                    valuePropName:'checked',
+                                    initialValue: true
+                                })(
+                                    <Switch checkedChildren="开" unCheckedChildren="关"/>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="生日" {...formItemLayout}>
+                            {
+                                getFieldDecorator('birthday',{
+                                    initialValue:moment('2018-08-08')
+                                })(
+                                    <DatePicker
+                                        showTime
+                                        // format="YYYY-MM-DD HH:mm:ss"
+                                        format="YYYY-MM-DD"
+                                    />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="联系地址" {...formItemLayout}>
+                            {
+                                getFieldDecorator('address',{
+                                    initialValue:'北京市海淀区奥林匹克公园'
+                                })(
+                                    <TextArea
+                                        autosize={rowObject}
+                                    />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="早起时间" {...formItemLayout}>
+                            {
+                                getFieldDecorator('time')(
+                                    <TimePicker/>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem label="头像" {...formItemLayout}>
+                            {
+                                getFieldDecorator('userImg')(
+                                    <Upload
+                                        listType="picture-card"
+                                        showUploadList={false}
+                                        action="//jsonplaceholder.typicode.com/posts/"
+                                        onChange={this.handleChange}
+                                    >
+                                    {this.state.userImg?<img src={this.state.userImg}/>:<Icon type="plus"/>}
+                                    </Upload>
+                                )
+                            }
+                        </FormItem>
+                    </Form>
+                </Card>
             </div>
         )
     }
