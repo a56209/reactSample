7f935cca169e1053b6bf70773927aa97defefc94
a56209 <41112394@qq.com>
2019-08-17 12:10:29 +0800

    查询控件封装

--- src/components/BaseForm/index.js
+++ src/components/BaseForm/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { Input, Form, Select } from 'antd';
+import { Input, Form, Select, DatePicker, Checkbox } from 'antd';
 import Utils from '../../utils/utils';
+import { Button } from 'antd/lib/radio';
 
 const FormItem = Form.Item;
 
 class FilterForm extends Component {
+
+    handleFilterSubmit = ()=>{
+        let fieldsValue = this.props.form.getFieldsValue(); //获取表单的值
+        this.props.filterSubmit(fieldsValue); //将子组件的值传递到父组件（order.js等）
+    }
+
+    reset = () => {
+        this.props.form.resetFields(); //重置表单的方法
+    }
 
     initFormList = () => {
         const { getFieldDecorator } = this.props.form;
@@ -29,18 +39,96 @@ class FilterForm extends Component {
                                     style={{ width: width }}
                                     placeholder={placeholder}
                                 >
-                                    {Utils.getOptionList([{ id: '0', name: '全部' }, { id: '1', name: '北京' }, { id: '2', name: '上海' }, { id: '3', name: '天津' }, { id: '4', name: '杭州' }])}
+                                    {Utils.getOptionList(item.list)}
                                 </Select>
                             )
                         }
                     </FormItem>
+                    formItemList.push(SELECT);
+                } else if (item.type == '时间查询') {
+                    const begin_time = <FormItem label="订单时间" key={field}>
+                        {
+                            getFieldDecorator('begin_time')(
+                                <DatePicker showTime={true} placeholder={placeholder} format="YYYY-MM-DD HH:mm:ss" />
+                            )
+                        }
+                    </FormItem>
+                    formItemList.push(begin_time);
+
+                    const end_time = <FormItem label="~" colon={false} key={field}>
+                        {
+                            getFieldDecorator('end_time')(
+                                <DatePicker showTime={true} placeholder={placeholder} format="YYYY-MM-DD HH:mm:ss" />)
+                        }
+                    </FormItem>
+                    formItemList.push(end_time);
+                } else if (item.type == 'INPUT') {
+                    const INPUT = <FormItem label={label} key={field}>
+                        {
+                            getFieldDecorator([field], {
+                                initialValue: initialValue
+                            })(
+                                <input type='text' style={{ width: width }} placeholder={placeholder} />
+                            )
+                        }
+                    </FormItem>
+                    formItemList.push(INPUT);
+                } else if (item.type == 'CHECKBOX') {
+                    const CHECKBOX = <FormItem label={label} key={field}>
+                        {
+                            getFieldDecorator([field], {
+                                valuePropName: 'checked', //设置checkbox的属性
+                                initialValue: initialValue //true|false
+                            })(
+                                <Checkbox>
+                                    {label}
+                                </Checkbox>
+                            )
+                        }
+                    </FormItem>
+                    formItemList.push(CHECKBOX);
+                } else if (item.type == '城市') {
+                    const city = <FormItem label="城市" key={field}>
+                        {
+                            getFieldDecorator('city', {
+                                initialValue: '0'
+                            })(
+                                <Select
+                                    style={{ width: 80 }}
+                                    placeholder={placeholder}
+                                >
+                                    {Utils.getOptionList([{ id: '0', name: '全部' }, { id: '1', name: '北京' }, { id: '2', name: '上海' }, { id: '3', name: '天津' }, { id: '4', name: '杭州' }])}
+                                </Select>
+                            )
+                        }
+                    </FormItem>;
+                    formItemList.push(city);
+                } else if (item.type == 'DATE') {
+                    const Date = <FormItem label={label} key={field}>
+                        {
+                            getFieldDecorator([field])(
+                                <DatePicker showTime={true} placeholder={placeholder} format="YYYY-MM-DD HH:mm:ss" />
+                            )
+                        }
+                    </FormItem>;
+                    formItemList.push(Date);
                 }
+
             });
         }
+        return formItemList;
     }
 
+
     render() {
-        return (<Form></Form>)
+        return (
+            <Form layout="inline">
+                <FormItem>
+                    <Button type="primary" style={{ margin: '0 20px' }} onClick={this.handleFilterSubmit}>查询</Button>
+                    <Button onClick={this.reset}>重置</Button>
+                </FormItem>
+            </Form>
+        )
     }
 
 }
